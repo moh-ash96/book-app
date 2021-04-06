39bebac8847ddb4538dd971708e103dbe70d1721
Muhannad <Muhannadalmughrabi233@gmail.com>
2021-04-06 16:34:58 +0300

    last edit for heroku

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 const pg = require('pg');
+const DATABASE_URL = process.env.DATABASE_URL;
+const NODE_ENV = process.env.NODE_ENV;
 
+const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
 // App setup 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(options);
 client.on('error', (err) => console.log(err));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
